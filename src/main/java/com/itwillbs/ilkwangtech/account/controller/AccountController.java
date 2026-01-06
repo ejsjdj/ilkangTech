34e93871c12517bfbcc24b79ceb3de290644e3aa
김기범 <ejsjdj@naver.com>
2026-01-06 17:50:02 +0900

    <KGB> 의존성 설정 및 스프링부트 초기설정

---
+++ src/main/java/com/itwillbs/ilkwangtech/account/controller/AccountController.java
@@ -0,0 +1,18 @@
+package com.itwillbs.ilkwangtech.account.controller;
+
+import com.itwillbs.ilkwangtech.account.dto.AccountDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class AccountController {
+
+    @PostMapping("/account/join")
+    public ResponseEntity<?> join(@RequestBody AccountDTO.JoinRequest joinRequest) {
+        return null;
+    }
+
+}

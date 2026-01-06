987db2067a9ece96c6ad6d924fb76490017d60c4
김기범 <ejsjdj@naver.com>
2026-01-06 15:58:55 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
@@ -0,0 +1,13 @@
+package com.itwillbs.ilkwangtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlkwangTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlkwangTechApplication.class, args);
+    }
+
+}

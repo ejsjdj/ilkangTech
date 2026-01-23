02b13831fd62f5afd3642571522dc467317f21d0
hhh88261 <hhh88261@gmail.com>
2026-01-23 17:38:45 +0900

    feat: commute controller

---
+++ src/main/java/com/itwillbs/ilkwangtech/Hr/dto/CommuteDTO.java
@@ -0,0 +1,30 @@
+package com.itwillbs.ilkwangtech.Hr.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CommuteDTO {
+    private long memberId; // 사번
+    private String depertment; // 부서
+    private LocalDateTime inTime; // 출근 시간
+    private LocalDateTime goOutTime; // 퇴근 시간
+    private LocalDateTime outTime;// 외근 시간
+    private LocalDateTime returnTime; //복귀 시간
+
+    @Builder
+    public CommuteDTO(long memberId, String depertment, LocalDateTime inTime, LocalDateTime goOutTime, LocalDateTime outTime, LocalDateTime returnTime){
+        super();
+        this.memberId = memberId;
+        this.depertment = depertment;
+        this.inTime = inTime;
+        this.goOutTime = goOutTime;
+        this.outTime = outTime;
+        this.returnTime = returnTime;
+  }
+}
--- src/main/java/com/itwillbs/ilkwangtech/Hr/repository/AttendanceRepository.java
+++ src/main/java/com/itwillbs/ilkwangtech/Hr/repository/AttendanceRepository.java
@@ -1,15 +1,19 @@
 package com.itwillbs.ilkwangtech.Hr.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+import com.itwillbs.ilkwangtech.Hr.dto.CommuteDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.itwillbs.ilkwangtech.Hr.entity.Attendance;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-	
+
 	// 사원 번호와 날짜(오늘)로 근태 기록 조회
     Optional<Attendance> findByMemberIdAndWorkDate(Long memberId, LocalDate workDate);
+
+    List<Attendance> findByMemberId(Long userId);
 
 }

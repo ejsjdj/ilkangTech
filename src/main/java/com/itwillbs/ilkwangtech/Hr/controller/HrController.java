78a83be66969a3ea2d223eacebfbbca2b566b32b
hhh88261 <hhh88261@gmail.com>
2026-01-26 17:40:29 +0900

    feat: add appointment controller

--- src/main/java/com/itwillbs/ilkwangtech/Hr/controller/HrController.java
+++ src/main/java/com/itwillbs/ilkwangtech/Hr/controller/HrController.java
@@ -1,33 +1,34 @@
 package com.itwillbs.ilkwangtech.Hr.controller;
 
-
+import com.itwillbs.ilkwangtech.Hr.dto.AppointmentDTO;
+import com.itwillbs.ilkwangtech.Hr.service.AppointmentService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 // 인사관리 메뉴탭
 @Controller
 @RequestMapping("/hr")
+@AllArgsConstructor
 public class HrController {
 
-//  인사 조회
-    @GetMapping("/emp")
-    public String getMember(){
-        return "인사조회";
-    }
+    private final AppointmentService appointmentService;
 
 //  발령 관리
     @GetMapping("/appointment")
-    public String getAppointment(){
-        return "hr/appointment";
+    @ResponseBody
+    public List<AppointmentDTO> getAppointment(){
+        return appointmentService.hrAppointmentService();
     }
 
 //  발령 등록
     @GetMapping("/appointment/insert")
     public String getAppointmentInsert(){
-
-
-
         return "hr/appointmentInsert";
     }
 

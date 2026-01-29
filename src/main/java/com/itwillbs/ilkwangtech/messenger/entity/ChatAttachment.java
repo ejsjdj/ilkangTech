ad24d7a29a4f7009c35710daa34799e666a63dc0
Mojiloo <lhjghwns@naver.com>
2026-01-29 17:24:53 +0900

    LHJ) 부가기능 엔티티 추가

---
+++ src/main/java/com/itwillbs/ilkwangtech/messenger/entity/ChatAttachment.java
@@ -0,0 +1,60 @@
+package com.itwillbs.ilkwangtech.messenger.entity;
+
+import java.time.LocalDateTime;
+
+import com.itwillbs.ilkwangtech.member.entity.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "chat_attachment")
+@Getter @Setter @NoArgsConstructor
+public class ChatAttachment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "msg_id", nullable = false)
+    private ChatMessage message;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private ChatRoom room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "original_name", nullable = false)
+    private String originalName;
+
+    @Column(name = "stored_name", nullable = false, unique = true)
+    private String storedName;
+
+    @Column(name = "store_path", nullable = false)
+    private String storePath;
+
+    @Column(name = "mime_type", nullable = false)
+    private String mimeType;
+
+    @Column(name = "file_size", nullable = false)
+    private Long fileSize;
+
+    @Column(name = "is_image", nullable = false, length = 1)
+    private String isImage = "N";
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
---
+++ src/main/java/com/itwillbs/ilkwangtech/messenger/entity/ChatReadStateId.java
@@ -0,0 +1,21 @@
+package com.itwillbs.ilkwangtech.messenger.entity;
+
+import java.io.Serializable;
+
+import groovy.transform.EqualsAndHashCode;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode
+public class ChatReadStateId implements Serializable {
+    @Column(name = "room_id")
+    private Long roomId;
+
+    @Column(name = "member_id")
+    private Long memberId;
+}

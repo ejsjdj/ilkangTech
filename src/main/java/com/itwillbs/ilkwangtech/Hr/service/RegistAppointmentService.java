c1960df998b9d23e302aaf573e01f947819092e0
hhh88261 <hhh88261@gmail.com>
2026-01-29 12:11:25 +0900

    feat: appointment complete

--- src/main/java/com/itwillbs/ilkwangtech/Hr/service/RegistAppointmentService.java
+++ src/main/java/com/itwillbs/ilkwangtech/Hr/service/RegistAppointmentService.java
@@ -37,6 +37,12 @@ public class RegistAppointmentService {
             appointmentEntity.newDept(newDept);
 
             member.setDepartment(newDept);
+
+        } else {
+
+            appointmentEntity.newDept(member.getDepartment());
+
+            appointmentEntity.changeDept(member.getDepartment());
         }
 
         // 2. 직급 등록
@@ -47,6 +53,13 @@ public class RegistAppointmentService {
             appointmentEntity.newRank(newRank);
 
             member.setPosition(newRank);
+
+        } else {
+
+            appointmentEntity.newRank(member.getPosition());
+
+            appointmentEntity.changeRank(member.getPosition());
+
         }
 
         // 3. 근무상태 등록
@@ -63,5 +76,7 @@ public class RegistAppointmentService {
         appointmentEntity.newApprover(approver);
 
         appointmentRepostiory.save(appointmentEntity);
+
+        System.out.println("발령 등록 완료!");
     }
 }

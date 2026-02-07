050826cf0e56e74868695231ede5011dbfff1bfd
김기범 <ejsjdj@naver.com>
2026-02-07 23:11:03 +0900

    feat : 캘린더 사진출력 구현

--- src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
+++ src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
@@ -10,7 +10,7 @@ public class IlkwangTechApplication {
     public static void main(String[] args) {
         SpringApplication.run(IlkwangTechApplication.class, args);
         System.out.println("🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌");
-        System.out.println("스프링프로젝트 실행");
+        System.out.println("🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌스프링프로젝트 실행🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌");
         System.out.println("🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌");
     }
 }

38758bf6766bee702139bf4fa792ee7bf9f0151e
ejsjdj <ejsjdj@naver.com>
2026-02-27 17:07:16 +0900

    왜안됨?

--- src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
+++ src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
@@ -1,13 +1,13 @@
 package com.itwillbs.ilkwangtech;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
+@MapperScan("com.itwillbs.ilkwangtech.**.mapper")
 public class IlkwangTechApplication {
 
     public static void main(String[] args) {

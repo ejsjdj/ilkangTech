1d310316b0cfeb743fa8fe24a584927fc3823849
김기범 <ejsjdj@naver.com>
2026-03-02 00:50:11 +0900

    feat : BOM 랜더링완료

--- src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
+++ src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
@@ -1,13 +1,11 @@
 package com.itwillbs.ilkwangtech;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
-@MapperScan("com.itwillbs.ilkwangtech.sales.mapper")
 public class IlkwangTechApplication {
 
     public static void main(String[] args) {

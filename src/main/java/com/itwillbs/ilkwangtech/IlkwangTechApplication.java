9b29aa19309e81ba9eacd56660c83165f7b6194e
김기범 <ejsjdj@naver.com>
2026-03-04 03:59:15 +0900

    feat : BOM 연결

--- src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
+++ src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
@@ -7,14 +7,10 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 @SpringBootApplication
 public class IlkwangTechApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(IlkwangTechApplication.class, args);
         System.out.println("🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌");
         System.out.println("🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌스프링프로젝트 실행🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌");
         System.out.println("🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌");
-
     }
-
 }

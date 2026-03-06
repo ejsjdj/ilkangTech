1ca00c3ebc4aebdad11e707be5a7ddfd77617c5a
ejsjdj <ejsjdj@naver.com>
2026-03-06 17:40:56 +0900

    수주기능 구현

--- src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
+++ src/main/java/com/itwillbs/ilkwangtech/IlkwangTechApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 @SpringBootApplication
 public class IlkwangTechApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(IlkwangTechApplication.class, args);
         System.out.println("🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌");
         System.out.println("🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌스프링프로젝트 실행🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌🙌");

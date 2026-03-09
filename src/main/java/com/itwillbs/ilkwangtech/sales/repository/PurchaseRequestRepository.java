d11d368558bc69cbce1c98686ec7bdcfb971c939
gnladu07 <gnlaud07@gmail.com>
2026-03-09 10:47:15 +0900

    YHM) 로직 수정

--- src/main/java/com/itwillbs/ilkwangtech/sales/repository/PurchaseRequestRepository.java
+++ src/main/java/com/itwillbs/ilkwangtech/sales/repository/PurchaseRequestRepository.java
@@ -5,6 +5,7 @@ import com.itwillbs.ilkwangtech.sales.entity.PurchaseRequestEntity;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,5 +19,10 @@ public interface PurchaseRequestRepository extends JpaRepository<PurchaseRequest
 	// 전체 발주대기량 그룹화 조회
     @Query("SELECT pr.item.itemId, COALESCE(SUM(pr.quantity), 0) FROM PurchaseRequestEntity pr GROUP BY pr.item.itemId")
     List<Object[]> sumQuantityGrouped();
+    
+    // 입고 완료된 품목의 발주 요청 데이터 삭제
+    @Modifying
+    @Query("DELETE FROM PurchaseRequestEntity pr WHERE pr.item.itemId = :itemId")
+    void deleteByItemId(@Param("itemId") Long itemId);
 
 }
